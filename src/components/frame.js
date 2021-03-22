a9fae5ec8e84a557f2ed892e314d3e13f786df5d
Johnnie Regalado <johnniereg@gmail.com>
2021-03-21 21:53:55 -0400

    Rough details overlay styling and set-up

--- src/components/frame.js
+++ src/components/frame.js
@@ -13,12 +13,15 @@ class Frame extends Component {
     super(props);
     this.state = {
       ...this.props.data,
+      showDetails: false,
       visible: false,
       zIndex: 0,
     };
 
     this.handleFrameClick = this.handleFrameClick.bind(this);
+    this.hideFrame = this.hideFrame.bind(this);
     this.sendToBack = this.sendToBack.bind(this);
+    this.toggleDetails = this.toggleDetails.bind(this);
     this.toggleFrame = this.toggleFrame.bind(this);
   }
 
@@ -40,12 +43,25 @@ class Frame extends Component {
     PubSub.publish("sendToBack", this.state.key);
   }
 
+  hideFrame() {
+    this.setState({
+      visible: false,
+      zIndex: 0,
+    });
+  }
+
   sendToBack(msg, data) {
     if (data !== this.state.key) {
       this.setState({
         zIndex: 0,
       });
     }
+  }
+
+  toggleDetails() {
+    this.setState((prevState) => ({
+      showDetails: !prevState.showDetails,
+    }));
   }
 
   toggleFrame(msg, data) {
@@ -85,59 +101,75 @@ class Frame extends Component {
           style={{
             zIndex: this.state.zIndex,
             backgroundColor: "transparent",
-            maxHeight: "50%",
-            maxWidth: "50%",
+            maxHeight: "33%",
+            maxWidth: "33%",
           }}
         >
-          <Carousel
-            dynamicHeight={true}
-            renderArrowPrev={(onClickHandler, hasPrev, label) =>
-              hasPrev && (
-                <button
-                  type="button"
-                  onClick={onClickHandler}
-                  title={label}
-                  style={{ ...arrowStyles, left: 15 }}
-                >
-                  -
-                </button>
-              )
+          <div
+            className={
+              this.state.showDetails ? "Details" : "Details Details--hidden"
             }
-            renderArrowNext={(onClickHandler, hasNext, label) =>
-              hasNext && (
-                <button
-                  type="button"
-                  onClick={onClickHandler}
-                  title={label}
-                  style={{ ...arrowStyles, right: 15 }}
-                >
-                  +
-                </button>
-              )
-            }
-            showIndicators={false}
-            showStatus={false}
-            showThumbs={false}
-            swipeable={false}
+            style={{
+              zIndex: this.state.zIndex + 2,
+            }}
           >
-            <img src={gif1} draggable="false" />
-            <img
-              src="https://via.placeholder.com/300/0000FF/808080?Text=Slide-1"
-              draggable="false"
-            />
-            <img
-              src="https://via.placeholder.com/300/FF0000/FFFFFF?Text=Slide-2"
-              draggable="false"
-            />
-            <img
-              src="https://via.placeholder.com/300/FFFF00/000000?Text=Slide-3"
-              draggable="false"
-            />
-            <img
-              src="https://via.placeholder.com/300/000000/FFFFFF/?text=Slide-4"
-              draggable="false"
-            />
-          </Carousel>
+            <div>Title</div>
+            <div>Description</div>
+          </div>
+          <div className="Controls" style={{ zIndex: this.state.zIndex + 3 }}>
+            <button onClick={this.hideFrame}>Close</button>
+            <button onClick={this.toggleDetails}>Details</button>
+          </div>
+          <div style={{ width: "300px" }}>
+            <Carousel
+              renderArrowPrev={(onClickHandler, hasPrev, label) =>
+                hasPrev && (
+                  <button
+                    type="button"
+                    onClick={onClickHandler}
+                    title={label}
+                    style={{ ...arrowStyles, left: 15 }}
+                  >
+                    -
+                  </button>
+                )
+              }
+              renderArrowNext={(onClickHandler, hasNext, label) =>
+                hasNext && (
+                  <button
+                    type="button"
+                    onClick={onClickHandler}
+                    title={label}
+                    style={{ ...arrowStyles, right: 15 }}
+                  >
+                    +
+                  </button>
+                )
+              }
+              showIndicators={false}
+              showStatus={false}
+              showThumbs={false}
+              swipeable={false}
+            >
+              <img src={gif1} draggable="false" />
+              <img
+                src="https://via.placeholder.com/300/0000FF/808080?Text=Slide-1"
+                draggable="false"
+              />
+              <img
+                src="https://via.placeholder.com/300/FF0000/FFFFFF?Text=Slide-2"
+                draggable="false"
+              />
+              <img
+                src="https://via.placeholder.com/300/FFFF00/000000?Text=Slide-3"
+                draggable="false"
+              />
+              <img
+                src="https://via.placeholder.com/300/000000/FFFFFF/?text=Slide-4"
+                draggable="false"
+              />
+            </Carousel>
+          </div>
         </div>
       </Draggable>
     );
