84eb92bcb9108bbf6f4621fbafc9b003f2428da2
Johnnie Regalado <johnniereg@gmail.com>
2021-03-21 21:21:04 -0400

    Z-index adjustment on click and custom carousel buttons

--- src/components/frame.js
+++ src/components/frame.js
@@ -17,20 +17,35 @@ class Frame extends Component {
       zIndex: 0,
     };
 
+    this.handleFrameClick = this.handleFrameClick.bind(this);
+    this.sendToBack = this.sendToBack.bind(this);
     this.toggleFrame = this.toggleFrame.bind(this);
   }
 
   componentDidMount() {
-    console.log(this.state);
     PubSub.subscribe("toggleFrame", this.toggleFrame);
+    PubSub.subscribe("sendToBack", this.sendToBack);
   }
 
   componentWillUnmount() {
     PubSub.unsubscribeAll();
   }
 
-  handleFrameClick(e) {
-    console.log(e.target);
+  handleFrameClick() {
+    // Bring clicked frame to front
+    this.setState({
+      zIndex: 10,
+    });
+    // Send other frames to back
+    PubSub.publish("sendToBack", this.state.key);
+  }
+
+  sendToBack(msg, data) {
+    if (data !== this.state.key) {
+      this.setState({
+        zIndex: 0,
+      });
+    }
   }
 
   toggleFrame(msg, data) {
@@ -47,46 +62,82 @@ class Frame extends Component {
   }
 
   render() {
+    const arrowStyles = {
+      position: "absolute",
+      zIndex: this.state.zIndex + 1,
+      bottom: "10px",
+      width: 30,
+      height: 30,
+      cursor: "pointer",
+    };
+
     return (
       <Draggable
         axis="both"
+        defaultPosition={{ x: 10, y: 10 }}
         handle=".Frame"
-        defaultPosition={{ x: 0, y: 0 }}
+        onMouseDown={this.handleFrameClick}
         position={null}
         scale={1}
-        onClick={this.handleFrameClick}
       >
         <div
           className={this.state.visible ? "Frame" : "Frame Frame--hidden"}
-          style={{ zIndex: this.state.zIndex, backgroundColor: "transparent" }}
+          style={{
+            zIndex: this.state.zIndex,
+            backgroundColor: "transparent",
+            maxHeight: "50%",
+            maxWidth: "50%",
+          }}
         >
-          <div style={{ width: "500px" }}>
-            <Carousel
-              dynamicHeight={true}
-              showIndicators={false}
-              showStatus={false}
-              showThumbs={false}
-              swipeable={false}
-            >
-              <img src={gif1} draggable="false" />
-              <img
-                src="https://via.placeholder.com/300/0000FF/808080?Text=Digital.com"
-                draggable="false"
-              />
-              <img
-                src="https://via.placeholder.com/300/FF0000/FFFFFF?Text=Down.com"
-                draggable="false"
-              />
-              <img
-                src="https://via.placeholder.com/300/FFFF00/000000?Text=WebsiteBuilders.com"
-                draggable="false"
-              />
-              <img
-                src="https://via.placeholder.com/300/000000/FFFFFF/?text=IPaddress.net"
-                draggable="false"
-              />
-            </Carousel>
-          </div>
+          <Carousel
+            dynamicHeight={true}
+            renderArrowPrev={(onClickHandler, hasPrev, label) =>
+              hasPrev && (
+                <button
+                  type="button"
+                  onClick={onClickHandler}
+                  title={label}
+                  style={{ ...arrowStyles, left: 15 }}
+                >
+                  -
+                </button>
+              )
+            }
+            renderArrowNext={(onClickHandler, hasNext, label) =>
+              hasNext && (
+                <button
+                  type="button"
+                  onClick={onClickHandler}
+                  title={label}
+                  style={{ ...arrowStyles, right: 15 }}
+                >
+                  +
+                </button>
+              )
+            }
+            showIndicators={false}
+            showStatus={false}
+            showThumbs={false}
+            swipeable={false}
+          >
+            <img src={gif1} draggable="false" />
+            <img
+              src="https://via.placeholder.com/300/0000FF/808080?Text=Slide-1"
+              draggable="false"
+            />
+            <img
+              src="https://via.placeholder.com/300/FF0000/FFFFFF?Text=Slide-2"
+              draggable="false"
+            />
+            <img
+              src="https://via.placeholder.com/300/FFFF00/000000?Text=Slide-3"
+              draggable="false"
+            />
+            <img
+              src="https://via.placeholder.com/300/000000/FFFFFF/?text=Slide-4"
+              draggable="false"
+            />
+          </Carousel>
         </div>
       </Draggable>
     );
