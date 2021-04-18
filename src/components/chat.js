5076b9edcf72fe8d61ccfb0a2fbb522f03ce073a
Johnnie Regalado <johnniereg@gmail.com>
2021-04-17 20:18:39 -0400

    Chat is doing its thing

--- src/components/chat.js
+++ src/components/chat.js
@@ -4,7 +4,59 @@ import Draggable from "react-draggable";
 class Chat extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      userName: "Lounge Visitor",
+      textToSubmit: "",
+      chatLog: [{ user: "Plintor Drax", text: "Hello" }],
+    };
+
+    this.onSubmit = this.onSubmit.bind(this);
+    this.handleUsernameChange = this.handleUsernameChange.bind(this);
+    this.handleTextInputChange = this.handleTextInputChange.bind(this);
+    this.messagesEndRef = React.createRef();
+  }
+
+  componentDidMount() {
+    this.scrollToBottom();
+  }
+  componentDidUpdate() {
+    this.scrollToBottom();
+  }
+  scrollToBottom = () => {
+    this.messagesEndRef.current.scrollIntoView({ behavior: "smooth" });
+  };
+
+  onSubmit(e) {
+    e.preventDefault();
+    const logEntry = {
+      user: this.state.userName,
+      text: this.state.textToSubmit,
+    };
+    this.setState((prevState) => ({
+      chatLog: [...prevState.chatLog, logEntry],
+      textToSubmit: "",
+    }));
+    setTimeout(() => {
+      this.botReply();
+    }, 1000);
+  }
+
+  handleUsernameChange(e) {
+    this.setState({ userName: e.target.value });
+  }
+
+  handleTextInputChange(e) {
+    this.setState({ textToSubmit: e.target.value });
+  }
+
+  botReply() {
+    const logEntry = {
+      user: "Plintor Drax",
+      text: "This is the next text for PD",
+    };
+    this.setState((prevState) => ({
+      chatLog: [...prevState.chatLog, logEntry],
+    }));
   }
 
   render() {
@@ -16,14 +68,37 @@ class Chat extends Component {
         position={null}
         scale={1}
       >
-        <div className="Chat">
+        <form className="Chat" onSubmit={this.onSubmit}>
           <h1 className="Chat__Heading">Chat with the Bot</h1>
-          <div className="Chat__Content"></div>
-          <div className="Chat__Input">
-            <input type="text"></input>
-            <button>Send</button>
+          <div className="Chat__UserName">
+            <input
+              name="Username"
+              type="text"
+              onChange={this.handleUsernameChange}
+              value={this.state.userName}
+            ></input>
+            <label htmlFor="Username">Name</label>
           </div>
-        </div>
+          <div className="Chat__Content">
+            {this.state.chatLog.map((entry, index) => {
+              console.log(entry);
+              return (
+                <p key={index}>
+                  {entry.user}: {entry.text}
+                </p>
+              );
+            })}
+            <div ref={this.messagesEndRef}></div>
+          </div>
+          <div className="Chat__Input">
+            <input
+              type="text"
+              onChange={this.handleTextInputChange}
+              value={this.state.textToSubmit}
+            ></input>
+            <button type="submit">Send</button>
+          </div>
+        </form>
       </Draggable>
     );
   }
